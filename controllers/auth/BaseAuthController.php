03519084952158a971ae5fc6280331b5fb3eb6bf
jeromepeng <snowxbear@163.com>
2017-10-17 20:03:33 +0800

    task: make url pretty

---
+++ controllers/auth/BaseAuthController.php
@@ -0,0 +1,21 @@
+<?php  
+/** 
+ * Created by PhpStorm. 
+ * User: Administrator 
+ * Date: 2016/3/16 
+ * Time: 23:03 
+ */  
+  
+namespace app\controllers\auth;  
+  
+  
+use yii\web\Controller;  
+  
+class BaseAuthController extends Controller
+{    
+    public function actionTest()
+    {  
+        return "test";
+    }  
+  
+}  
